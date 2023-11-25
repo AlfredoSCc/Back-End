4ad908692cc74786c1f4bb440605ac9db8af9b17
Alfredo Castro Colmenares <128981882+AlfredoSCc@users.noreply.github.com>
2023-11-24 23:39:57 -0500

    prueba de codigo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 //Importación de Paquetes
-const express = require("express");
+const express = require("express")
 
 
 
 //Definir Configuraciones
-
+//Inicializamos el paquete express
+const app = express();
 
 
 //Ejecución
+
+app.get("/prueba", function (req,res){
+    res.send("Hola, estas en la web de prueba")
+})
+
+//Puerto en el que Escuchara o trabajara nuestra pagina
+
+app.listen(3000);
